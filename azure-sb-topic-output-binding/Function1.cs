6a3466f6502c21daf467fa8ab43017a714cf261d
Hemant <hemant.shelar@gmail.com>
2025-02-04 22:14:28 +1100

    Complete example.

--- azure-sb-topic-output-binding/Function1.cs
+++ azure-sb-topic-output-binding/Function1.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using Microsoft.ServiceBus.Messaging;
 
 namespace azure_sb_topic_output_binding
 {
@@ -16,17 +19,44 @@ namespace azure_sb_topic_output_binding
         }
 
         [Function(nameof(Function1))]
-        public async Task Run(
+        [ServiceBusOutput("test-topic", Connection = "CONN")]
+        public async Task<OutputData> Run(
             [ServiceBusTrigger("test-queue", Connection = "CONN")]
             ServiceBusReceivedMessage message,
-            ServiceBusMessageActions messageActions)
+            ServiceBusMessageActions messageActions,
+            ICollector<BrokeredMessage> collector
+            )
         {
             _logger.LogInformation("Message ID: {id}", message.MessageId);
             _logger.LogInformation("Message Body: {body}", message.Body);
             _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
 
+
+            //Following return 
+            //Is there any way to set custom properties of this message?
+            //Along with custom property, I would also like to set messageProperty contentType to application/json
+
+            OutputData outputData = new OutputData
+            {
+                ID = 123,
+                Name = "Test"
+            };
+            return outputData;
+
+
+            //As per sloution mentioned in below link, I tried to set custom properties but it is not working.
+            //https://stackoverflow.com/questions/50457428/custom-message-properties-on-azure-queue-topic-message-from-azure-function
+
+            BrokeredMessage brokeredMessage = new();
+            brokeredMessage.ContentType = "application/json";
+            brokeredMessage.Properties.Add("ID", 123);
+            brokeredMessage.Properties.Add("Name", "Test");
+            //Injecting  ICollector<BrokeredMessage>  notworking as its always null.
+            //collector.Add(brokeredMessage);
             // Complete the message
             await messageActions.CompleteMessageAsync(message);
+
+
         }
     }
 }
