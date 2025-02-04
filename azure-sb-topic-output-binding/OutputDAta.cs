cbc14d6a9b5569e3bee8b5b58d943957a726fa36
Hemant <hemantshelar@users.noreply.github.com>
2025-02-04 22:19:14 +1100

    Feature/02 topic ouput (#1)
    * Initial project.
    
    * Complete example.

---
+++ azure-sb-topic-output-binding/OutputDAta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace azure_sb_topic_output_binding;
+    public class OutputData
+    {
+    public int ID { get; set; }
+    public string Name { get; set; }
+
+}
